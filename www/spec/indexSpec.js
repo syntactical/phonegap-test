3a7834bcf0ac522ed35469641e2e277a71088ffd
SWatro <Thoughtworker@SW9640.local>
2014-04-28 16:44:21 -0500

    stacey and greg | Removes bower_components and creates lib folder in www

--- www/spec/indexSpec.js
+++ www/spec/indexSpec.js
@@ -1,6 +1,6 @@
 describe("index",function(){
     it("should render second screen when button is clicked",function(){
-       app.button();
+
        expect(app.currentScreen).toBe("secondScreen.html");
     });
 })
